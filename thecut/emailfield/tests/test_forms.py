2f4c37e2789deda6eff0882ea13d72d6ce5ab475
Kye Russell <kye.russell@thecut.net.au>
2015-11-21 12:38:12 +0800

    Added tests for ``forms.EmailField`` and ``models.EmailField``. Refs #6002

--- thecut/emailfield/tests/test_forms.py
+++ thecut/emailfield/tests/test_forms.py
@@ -5,38 +5,33 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase
 from dns.exception import DNSException
 from mock import patch
-from test_app.models import EmailModel
+from test_app.forms import EmailForm
 from unittest import skipIf
 
 
 class TestEmailField(TestCase):
 
-    """Tests for the EmailField model field."""
+    """Tests for the EmailField form field."""
 
     @skipIf(DJANGO_VERSION >= (1, 5), 'This validation is handled by Django.')
     def test_rejects_address_without_at_sign(self):
         """Reject email addresses without an '@' sign."""
-        model = EmailModel(email='INVALID')
-        with self.assertRaises(ValidationError):
-            model.full_clean()
+        form = EmailForm(data={'email': 'INVALID'})
+        self.assertFalse(form.is_valid())
 
     @patch('dns.resolver.query')
     def test_rejects_domain_without_mx_record(self, fake_query):
         """Reject email address for a domain without an MX record."""
         fake_query.side_effect = DNSException()
-        model = EmailModel(email='development@thecut.net.au')
-        with self.assertRaises(ValidationError):
-            model.full_clean()
+        form = EmailForm(data={'email': 'development@thecut.net.au'})
+        self.assertFalse(form.is_valid())
         # Enure dns.resolver.query() was actually called.
         fake_query.assert_called_once_with('thecut.net.au', 'MX')
 
     @patch('dns.resolver.query')
     def test_accepts_domain_with_mx_record(self, fake_query):
         """Accept email address for a domain with an MX record."""
-        model = EmailModel(email='development@thecut.net.au')
-        try:
-            model.full_clean()
-        except ValidationError:
-            self.fail('Email address was incorrectly rejected.')
+        form = EmailForm(data={'email': 'development@thecut.net.au'})
+        self.assertTrue(form.is_valid())
         # Enure dns.resolver.query() was actually called.
         fake_query.assert_called_once_with('thecut.net.au', 'MX')
