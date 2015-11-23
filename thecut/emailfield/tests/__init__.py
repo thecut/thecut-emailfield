e9026eb74b57025521088672219610029e1fd523
Kye Russell <kye.russell@thecut.net.au>
2015-11-23 11:12:01 +0800

    Fixed incorrect Django version-specific import of tests in ``tests/__init__.py.

--- thecut/emailfield/tests/__init__.py
+++ thecut/emailfield/tests/__init__.py
@@ -2,6 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 from django import VERSION as DJANGO_VERSION
 
-if DJANGO_VERSION < (1, 4):
+if DJANGO_VERSION < (1, 6):
     from .test_forms import *  # NOQA
     from .test_models import *  # NOQA
