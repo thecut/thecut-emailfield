08435f496ea0ac2eaa6c61ac1ac978dd4ec48c12
Kye Russell <kye.russell@thecut.net.au>
2015-11-23 10:43:35 +0800

    Import all test files in ``tests/__init__.py`` - Django 1.4 compatibility

--- thecut/emailfield/tests/__init__.py
+++ thecut/emailfield/tests/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from django import VERSION as DJANGO_VERSION
+
+if DJANGO_VERSION < (1, 4):
+    from .test_forms import *  # NOQA
+    from .test_models import *  # NOQA
